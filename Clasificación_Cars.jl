5340b8ad7a6a0eb714682e6503dc609bd1401e4d
Antón Liñares López <antonlinares03@gmail.com>
2024-04-29 17:20:19 +0200

    claro

--- Clasificación_Cars.jl
+++ Clasificación_Cars.jl
@@ -4,9 +4,6 @@
 # de car + evaluation.
 
 # Primero cargamos la base de datos y la preprocesamos
-
-using CSV
-
 # download DataFrames package
 using Pkg
 Pkg.add("DataFrames")
@@ -14,44 +11,16 @@ Pkg.add("DataFrames")
 
 using DataFrames
 using Random
-using DecisionTree
 using ScikitLearn: fit!, predict, @sk_import
-using MLBase: cross_entropy
-using StatsBase: sample, mean
 using Plots
 using Statistics
-
-# Cargamos la base de datos
-# import the car.data file
-
-# Creamos un DataFrame con la base de datos
-# Creamos un DataFrame con la base de datos
-
-
-
-
-# Cargamos la base de datos
-# import the car.data file
-
 using Random
 using DelimitedFiles;
 
-using Statistics
-dataset = readdlm("datasets/iris.data", ','); # me pasas por wasap ou teams este archivo de iris? 
-
-df = CSV.read("datasets/iris.data", DataFrame; header = false)
 
 
-# Check if the file exists and is accessible
-import Base.Filesystem: isfile
+dataset = readdlm("datasets/car_evaluation/car.data", ','); # me pasas por wasap ou teams este archivo de iris? 
 
-if isfile("datasets/iris.data")
-    df = CSV.read("datasets/iris.data", DataFrame; header = false)
-else
-    println("File does not exist or is not accessible")
-end
-
-
-datasets/car_evaluation/car.data
-# Preprocesamos la base de datos
-# Convertimos las variables categóricas a numéricas
+# Creamos un DataFrame con la base de datos
+df = DataFrame(dataset, :auto)
+println(df)
